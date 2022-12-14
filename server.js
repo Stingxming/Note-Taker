59a33a4e0b9ce341d78b3cd0669836134c7bfa5e
Mathew Chandler <Cascabelrock@gmail.com>
2022-12-13 18:32:16 -0800

    debugged syntax error with routes

--- server.js
+++ server.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const path = require('path');
-//const { clog } = require('./middleware/clog');
-//const api = require('./routes/index.js');
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
-
-// Import custom middleware, "cLog"
-//app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+/** NAVIGATION ROUTES BEGINS */
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+); // ROUTES FOR HOME PAGE
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+); // GET Route for notes page
+
+/**NAVIGATION ROUTES END */
+
+/** API ROUTE BEGINS */
+
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
 );
 
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+/** API ROUTES ENDS */
 
-// Wildcard route to direct users to a main page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+); // Wildcard route to direct users to a main page
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+
+
