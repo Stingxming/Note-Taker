6da0b96ff7377859ef467040a46c2281e963d72f
Mathew Chandler <Cascabelrock@gmail.com>
2022-12-15 22:13:36 -0800

    set up post request, tested in insomnia

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const path = require("path");
 const db = require("./db/db.json");
+const {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} = require("./helpers/fsUtils");
 
 const PORT = 3001;
 
@@ -10,36 +15,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public")));
+/** API ROUTE BEGINS */
+
+app.get("/api/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+/** API ROUTES ENDS */
+
+app.use(express.static("public"));
 
 /** NAVIGATION ROUTES BEGINS */
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 ); // ROUTES FOR HOME PAGE
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 ); // GET Route for notes page
 
 /**NAVIGATION ROUTES END */
 
-/** API ROUTE BEGINS */
-
-app.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/db/db.json"))
-);
-
-/** API ROUTES ENDS */
-
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 ); // Wildcard route to direct users to a main page
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-
-
